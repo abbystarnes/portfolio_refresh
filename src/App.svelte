b95bf155e158fd72299ad9eab8fe627e37305d64
Abby Starnes <abbystarnes@Abbys-Air-3.attlocal.net>
2024-02-01 20:26:13 -0500

    feat[scaffolding]: under construction

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,32 @@
-<script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+<script>
+  import catConstruction from "./assets/catConstruction.png";
 </script>
 
 <main>
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+    <img src={catConstruction} class="logo" alt="Cat Under Construction" />
   </div>
-  <h1>Vite + Svelte</h1>
+  <h1>AMS Portfolio</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
-
+  <p>Under Construction</p>
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
+    Check out <a
+      href="https://abbystarnes.github.io/2017-portfolio/"
+      target="_blank"
+    >
+      my old portfolio site
+    </a>in the meantime.
   </p>
 </main>
 
 <style>
   .logo {
-    height: 6em;
+    height: 15em;
     padding: 1.5em;
     will-change: filter;
     transition: filter 300ms;
   }
   .logo:hover {
     filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
   }
 </style>
